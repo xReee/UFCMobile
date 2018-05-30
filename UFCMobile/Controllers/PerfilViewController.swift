4cadf77abbefd8cc426b9b49436d75bea7630325
renata <renata.faria.gomes@hotmail.com>
2018-05-30 10:02:24 -0300

    tela de perfil oj

--- UFCMobile/Controllers/PerfilViewController.swift
+++ UFCMobile/Controllers/PerfilViewController.swift
@@ -8,10 +8,23 @@
 
 import UIKit
 
-class PerfilViewController: BarraBrancaViewController {
+class PerfilViewController: BarraBrancaViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "perfilCell")
+        cell?.textLabel?.text = opcoes[indexPath.row]
+        return cell!
+    }
+    
 
     @IBOutlet weak var imgPerfil: UIImageView!
     @IBOutlet weak var btnEditar: UIButton!
+    
+    let opcoes = ["Perfil Completo", "Atestado de matrícula", "Cadeiras Anteriores", "IRA", "Resultado do processamento"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,12 +36,16 @@ class PerfilViewController: BarraBrancaViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewDidLayoutSubviews() {
+        setupViews()
+    }
+    
     func setupViews(){
         imgPerfil.layer.borderWidth = 10
         imgPerfil.layer.borderColor = UIColor.white.cgColor
-        imgPerfil.layer.cornerRadius = (imgPerfil.frame.size.width / 1.7)
+        imgPerfil.layer.cornerRadius = (imgPerfil.frame.size.width / 2)
         
-        btnEditar.layer.borderWidth = 5
+        btnEditar.layer.borderWidth = 1
         btnEditar.layer.borderColor = UIColor.white.cgColor
         btnEditar.layer.cornerRadius = btnEditar.frame.size.height / 3
     }
